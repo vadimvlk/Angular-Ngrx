cb16a5abd34f767fc2d3aab52aac7e6b12b98380
srvdev.volkov <v.volkov@globus.ru>
2021-05-12 10:25:19 +0300

    add counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+
+  counter: number = 0;
+
+  get isGreaterZero (): boolean {
+    return this.counter> 0
+  }
+
+  increase() {
+    this.counter++;
+  }
+
+  decrease() {
+    this.counter--;
+  }
+
+  resetCount() {
+    this.counter = 0;
+  }
 }
