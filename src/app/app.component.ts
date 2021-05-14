c53f50f5c28ba4284d54c37070e5965378735159
srvdev.volkov <v.volkov@globus.ru>
2021-05-14 10:27:43 +0300

    change property name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,29 +11,24 @@ import {map} from "rxjs/operators";
 })
 export class AppComponent {
 
-
   constructor(private _store: Store) {
 
   }
 
   updatedAt$: Observable<any> = this._store.select(changeUpdatedAt);
   count$: Observable<number> = this._store.select(counterSelector)
-  isGreaterZero$: Observable<boolean> = this.count$.pipe(
+  isDisallowDecrease$: Observable<boolean> = this.count$.pipe(
     map(x => x <= 0));
+
   increase() {
     this._store.dispatch(increase());
-
   }
 
   decrease() {
     this._store.dispatch(decrease());
-
-
   }
 
   resetCount() {
     this._store.dispatch(reset());
-
-
   }
 }
